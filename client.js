9f7a8e72008313623335ffbfc2ba24661948b32c
Craig Mulligan <hobochildster@gmail.com>
2020-06-06 14:02:20 +0200

    beta-0.9

--- client.js
+++ client.js
@@ -3,7 +3,9 @@
 require('./aws')
 const dataApi = require('data-api-client')
 if (!process.env.NAWR_SQL_CONNECTION) {
-  throw new Error('NAWR_SQL_CONNECTION is not set')
+  throw new Error(
+    `NAWR_SQL_CONNECTION is not set: You may need to run: $ nawr init`
+  )
 }
 const connectionValue = JSON.parse(process.env.NAWR_SQL_CONNECTION)
 const { secretArn, resourceArn, database } = connectionValue
