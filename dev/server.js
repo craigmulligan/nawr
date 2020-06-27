6646f88b15bce9cd5b39775b69d4807cd0339766
Craig Mulligan <hobochildster@gmail.com>
2020-06-27 12:33:53 +0200

    switch the dev command to event based logging

--- dev/server.js
+++ dev/server.js
@@ -25,16 +25,14 @@ module.exports = () => {
         })
         req.on('end', async () => {
           const evt = JSON.parse(body)
-          console.log(evt)
           try {
             await worker(query.name, evt).then(console.log)
           } catch (err) {
-            console.log('ERRR', err)
             res.writeHead(500, {
               'Content-Type': 'application/json',
               'X-Powered-By': 'nawr + next.js'
             })
-            res.write(JSON.stringify({ error: err }))
+            res.write(JSON.stringify({ error: err.message }))
             return res.end()
           }
 
